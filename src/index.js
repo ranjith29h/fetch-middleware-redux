28950224720f1f11be22956d013daeee6b3cf015
ranjith <ranjith@mquotient.net>
2018-01-06 18:27:34 +0530

    removed console on action

--- src/index.js
+++ src/index.js
@@ -3,26 +3,23 @@ import FetchAsyncApi from './asyncFetch';
 
 const api = ({ dispatch, getState }) => next => action => {
 
-
-    console.log(action, new Date().toLocaleString());
-
     if (!action.request) {
         return next(action);
     }
 
-   function actionToDispatch(actionType,actionData){
-       dispatch({type:actionType,payload:actionData});
-   }
+    function actionToDispatch(actionType, actionData) {
+        dispatch({ type: actionType, payload: actionData });
+    }
 
-    const validation = validate(action.request); 
+    const validation = validate(action.request);
     if (validation) {
         const getResponse = FetchAsyncApi.requestApi(action.request);
         getResponse.then(function (data) {
             if (data) {
-                return actionToDispatch(action.request.types[0],data);
+                return actionToDispatch(action.request.types[0], data);
             }
         }).catch(function (err) {
-            return actionToDispatch(action.request.types[1],err.message);
+            return actionToDispatch(action.request.types[1], err.message);
         });
     }
 
